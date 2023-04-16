00f8d72db6ba5ba53fc22d5b376399db47d28707
Rostislav <rostwik@gmail.com>
2023-04-16 23:00:19 +0300

    Refactor

--- telegram_bot_recognizer.py
+++ telegram_bot_recognizer.py
@@ -24,6 +24,10 @@ class TelegramLogsHandler(logging.Handler):
         self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
+def error_handler(update, context):
+    logger.error(f'Телеграм бот упал с ошибкой: {context.error}', exc_info=True)
+
+
 def detect_intent_texts(project_id, session_id, text, language_code='ru-RU'):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
@@ -59,13 +63,14 @@ def echo(update: Update, context: CallbackContext, google_project_id):
 def main():
     load_dotenv()
     telegram_api_token = os.getenv('TELEGRAM_API_TOKEN')
+    telegram_monitor_api_token = os.getenv('TELEGRAM_MONITOR_API_TOKEN')
     telegram_admin_chat_id = os.getenv('TELEGRAM_CHAT_ID')
     google_project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
 
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
     )
-    logger_bot = telegram.Bot(token=telegram_api_token)
+    logger_bot = telegram.Bot(token=telegram_monitor_api_token)
     logger.setLevel(logging.WARNING)
     logger.addHandler(TelegramLogsHandler(logger_bot, telegram_admin_chat_id))
 
@@ -78,18 +83,11 @@ def main():
         Filters.text & ~Filters.command,
         lambda update, context: echo(update, context, google_project_id))
     )
+    dispatcher.add_error_handler(error_handler)
 
-    while True:
-        try:
-            logger.info('Телеграм бот запущен.')
-            updater.start_polling()
-            updater.idle()
-
-        except requests.exceptions.ConnectionError:
-            logger.error('Интернет исчез')
-            time.sleep(5)
-        except Exception as err:
-            logger.error(f'Телеграм бот упал с ошибкой: {err}', exc_info=True)
+    logger.info('Телеграм бот запущен.')
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
--- vk_bot_recognizer.py
+++ vk_bot_recognizer.py
@@ -43,12 +43,12 @@ def main():
     load_dotenv()
     vk_api_token = os.getenv('VK_API_TOKEN')
     telegram_admin_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-    telegram_api_token = os.getenv('TELEGRAM_API_TOKEN')
+    telegram_monitor_api_token = os.getenv('TELEGRAM_MONITOR_API_TOKEN')
     google_project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
     )
-    logger_bot = telegram.Bot(token=telegram_api_token)
+    logger_bot = telegram.Bot(token=telegram_monitor_api_token)
     logger.setLevel(logging.WARNING)
     logger.addHandler(TelegramLogsHandler(logger_bot, telegram_admin_chat_id))
 
@@ -56,22 +56,23 @@ def main():
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
-    try:
-        logger.info('Бот запущен.')
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                response = detect_intent_texts(google_project_id, event.user_id, event.text)
-                if not response.query_result.intent.is_fallback:
-                    vk_api.messages.send(
-                        user_id=event.user_id,
-                        message=response.query_result.fulfillment_text,
-                        random_id=random.randint(1, 1000)
-                    )
-    except requests.exceptions.ConnectionError:
-        logger.error('Интернет исчез')
-        time.sleep(5)
-    except Exception as err:
-        logger.error(f'Бот упал с ошибкой: {err}', exc_info=True)
+    while True:
+        try:
+            logger.info('VK Бот запущен.')
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    response = detect_intent_texts(google_project_id, event.user_id, event.text)
+                    if not response.query_result.intent.is_fallback:
+                        vk_api.messages.send(
+                            user_id=event.user_id,
+                            message=response.query_result.fulfillment_text,
+                            random_id=random.randint(1, 1000)
+                        )
+        except requests.exceptions.ConnectionError:
+            logger.error('Интернет исчез')
+            time.sleep(5)
+        except Exception as err:
+            logger.error(f'VK Бот упал с ошибкой: {err}', exc_info=True)
 
 
 if __name__ == '__main__':
